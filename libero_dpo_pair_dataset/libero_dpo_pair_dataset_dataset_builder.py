6c0e693a195dd2e68768fd63b6b965fb9f3e3757
rooshy <yangrushuai@icloud.com>
2025-02-26 11:46:38 +0000

    Implement DPO pair dataset generation with success and failure trajectories
    - Add method to split dataset into success and failure trajectories
    - Modify _generate_examples to create DPO pairs from successful and failed episodes
    - Remove 'chosen_action_id' and 'rejected_action_id' text features
    - Ensure only successful episodes are included in the final dataset
    - Add error handling for mismatched episode lengths

--- libero_dpo_pair_dataset/libero_dpo_pair_dataset_dataset_builder.py
+++ libero_dpo_pair_dataset/libero_dpo_pair_dataset_dataset_builder.py
@@ -94,12 +94,6 @@ class LiberoDpoPairDataset(tfds.core.GeneratorBasedBuilder):
                         dtype=np.bool_,
                         doc='True if the episode is successful.'
                     ),
-                    'chosen_action_id': tfds.features.Text(
-                        doc='Path to the chosen action data file.'
-                    ),
-                    'rejected_action_id': tfds.features.Text(
-                        doc='Path to the rejected action data file.'
-                    ),
                 }),
             }))
 
@@ -111,32 +105,67 @@ class LiberoDpoPairDataset(tfds.core.GeneratorBasedBuilder):
             # 'val': self._generate_examples(path='data/val/episode_*.npy'), # TODO: add val set, currently only train set is used
         }
 
+    def split_success_and_false_trajectory(self, dataset: tf.data.Dataset):
+        """Split the dataset into success and false trajectory."""
+
+        def reduction_fn(episode):
+            total_reward = 0.0
+            for step in episode['steps']:
+                total_reward += step['reward']
+            episode_success = total_reward > 0
+            episode['success'] = episode_success
+            return episode
+
+        dataset_with_success_label = dataset.map(reduction_fn)
+        success_dataset = dataset_with_success_label.filter(lambda x: x['success'])
+        false_dataset = dataset_with_success_label.filter(lambda x: not x['success'])
+        def get_dataset_size(dataset):
+            size = 0
+            for _ in dataset:
+                size += 1
+            return size
+        total_size = get_dataset_size(dataset)
+        success_size = get_dataset_size(success_dataset)
+        false_size = get_dataset_size(false_dataset)
+
+        assert total_size == success_size + false_size, f"total_size: {total_size} != success_size: {success_size} + false_size: {false_size}"
+        return success_dataset, false_dataset, success_size > 0 and false_size > 0
+
     def _generate_examples(self, path) -> Iterator[Tuple[str, Any]]:
         """Generator of examples for each split."""
         num_steps_wait = 10    # Number of steps to wait for objects to stabilize in sim
-        def _parse_example(episode_path, episode, language_instruction):
+        def _parse_example(episode_path, true_episode, false_episode, language_instruction):
             success = False
             new_episode = []
-            for i, step in enumerate(episode['steps']):
-                if i < num_steps_wait:
+            false_action_steps = []
+            for step in false_episode['steps']:
+                false_action_steps.append(step['action'])
+            for i, success_step in enumerate(true_episode['steps']):
+                if i < num_steps_wait: # skip the first few steps to wait for objects to stabilize in sim
                     continue
+                if i >= len(false_action_steps): # if there are no more false action steps, break
+                    print(f"Warning: no more false action steps for {episode_path},"
+                          f" which means the false episode is too short.")
+                    break
+
                 # TODO: add language embedding
                 language_embedding = np.zeros(512, dtype=np.float32)
                 if not success:
-                    success = step['is_last'] or step['reward'] > 0
+                    success = success_step['is_last'] or success_step['reward'] > 0
 
                 new_episode.append({
                     'observation': {
-                        'image': step['observation']['agentview_image'].numpy(),
-                        'wrist_image': step['observation']['robot0_eye_in_hand_image'].numpy(),
-                        'state': step['observation']['robot0_proprio-state'].numpy(),
+                        'image': success_step['observation']['agentview_image'].numpy(),
+                        'wrist_image': success_step['observation']['robot0_eye_in_hand_image'].numpy(),
+                        'state': success_step['observation']['robot0_proprio-state'].numpy(),
                     },
-                    'action': step['action'].numpy(),
+                    'chosen_action': success_step['action'].numpy(),
+                    'rejected_action': false_action_steps[i].numpy(),
                     'discount': 1.0,
-                    'reward': step['reward'].numpy(),
-                    'is_first': step['is_first'].numpy(),
-                    'is_last': step['is_last'].numpy(),
-                    'is_terminal': step['is_terminal'].numpy(),
+                    'reward': success_step['reward'].numpy(),
+                    'is_first': success_step['is_first'].numpy(),
+                    'is_last': success_step['is_last'].numpy(),
+                    'is_terminal': success_step['is_terminal'].numpy(),
                     'language_instruction': language_instruction,
                     'language_embedding': language_embedding,
                 })
@@ -150,10 +179,8 @@ class LiberoDpoPairDataset(tfds.core.GeneratorBasedBuilder):
                 }
             }
             sample_id = episode_path + "_" + str(np.random.randint(0, 1000000))
-            if success:
-                return sample_id, sample
-            else:
-                return sample_id, None
+            assert success, f"dpo pair dataset should only contain successful episodes, but {episode_path} is not successful."
+            return sample_id, sample
 
         # create list of all examples
         task_paths = glob.glob(path)
@@ -165,12 +192,14 @@ class LiberoDpoPairDataset(tfds.core.GeneratorBasedBuilder):
             language_instruction = datasetbuilder.info.name
             dataset = datasetbuilder.as_dataset(split='train')
             # assemble episode --> here we're assuming demos so we set reward to 1 at the end
-
-            for i, episode in enumerate(dataset):
-                episode_path = language_instruction + f"/{i}"
-                sample_id, sample = _parse_example(episode_path, episode, language_instruction)
-                # NOTE: if you want to skip an example for whatever reason, simply return None, e.g. only success samples are used for training
-                if sample is not None:
+            success_dataset, false_dataset, legally_split = self.split_success_and_false_trajectory(dataset)
+            # ignore the task if it cannot be contructed into DPO pairs.
+            if not legally_split:
+                continue
+            for i, true_episode in enumerate(success_dataset):
+                for j, false_episode in enumerate(false_dataset):
+                    episode_path = language_instruction + f"/{i}_{j}"
+                    sample_id, sample = _parse_example(episode_path, true_episode, false_episode, language_instruction)
                     yield sample_id, sample
 
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
