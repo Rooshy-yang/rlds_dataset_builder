eef0b2bb63ef160c083e3cdc43230a8f32255aa5
rooshy <yangrushuai@icloud.com>
2025-03-14 07:13:56 +0000

    with image preprocessing and type adjustments
    - Introduce image extraction and preprocessing functions for agent and wrist images in LiberoDataset, LiberoDpoPairDataset, and LiberoFailureDataset.
    - Update action data type from float64 to float32 for consistency across datasets.
    - Modify episode generation to ensure the final state is removed and validated.

--- libero_dpo_pair_dataset/libero_dpo_pair_dataset_dataset_builder.py
+++ libero_dpo_pair_dataset/libero_dpo_pair_dataset_dataset_builder.py
@@ -5,7 +5,65 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 # import tensorflow_hub as hub
+import math
+from PIL import Image
 
+
+def get_libero_image(obs, resize_size, key="agentview_image"):
+    """Extracts image from observations and preprocesses it."""
+    assert isinstance(resize_size, int) or isinstance(resize_size, tuple)
+    if isinstance(resize_size, int):
+        resize_size = (resize_size, resize_size)
+    img = obs[key]
+    img = np.flipud(img)
+    # img = img[::-1, ::-1]  # IMPORTANT: rotate 180 degrees to match train preprocessing
+    img = Image.fromarray(img)
+    img = img.resize(resize_size, Image.Resampling.LANCZOS)  # resize to size seen at train time
+    img = img.convert("RGB")
+    return np.array(img)
+
+def apply_center_crop(im, t_h, t_w):
+    """
+    Source: https://github.com/ARISE-Initiative/robomimic/blob/5dee58f9cc1235010d0877142b54d0e82dd23986/robomimic/utils/obs_utils.py#L268
+
+    Takes a center crop of an image.
+
+    Args:
+        im (np.array or torch.Tensor): image of shape (..., height, width, channel)
+        t_h (int): height of crop
+        t_w (int): width of crop
+
+    Returns:
+        im (np.array or torch.Tensor): center cropped image
+    """
+    assert im.shape[-3] >= t_h and im.shape[-2] >= t_w
+    assert im.shape[-1] in [1, 3, 6]
+    crop_h = int((im.shape[-3] - t_h) / 2)
+    crop_w = int((im.shape[-2] - t_w) / 2)
+    return im[..., crop_h : crop_h + t_h, crop_w : crop_w + t_w, :]
+
+def preprocess_image(img):
+    image = Image.fromarray(img)
+    image = image.convert("RGB")
+
+    # (If trained with image augmentations) Center crop image and then resize back up to original size.
+    # IMPORTANT: Let's say crop scale == 0.9. To get the new height and width (post-crop), we must multiply
+    #            the original height and width by sqrt(0.9) -- not 0.9!
+
+    temp_image = np.array(image)  # (H, W, C)
+    crop_scale = 0.9
+    sqrt_crop_scale = math.sqrt(crop_scale)
+    temp_image_cropped = apply_center_crop(
+        temp_image,
+        t_h=int(sqrt_crop_scale * temp_image.shape[0]),
+        t_w=int(sqrt_crop_scale * temp_image.shape[1]),
+    )
+    temp_image = Image.fromarray(temp_image_cropped)
+    temp_image = temp_image.resize(
+        image.size, Image.Resampling.BILINEAR
+    )  # IMPORTANT: dlimp uses BILINEAR resize
+    image = temp_image
+    return image
 
 class LiberoDpoPairDataset(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for example dataset."""
@@ -152,15 +210,19 @@ class LiberoDpoPairDataset(tfds.core.GeneratorBasedBuilder):
                 language_embedding = np.zeros(512, dtype=np.float32)
                 if not success:
                     success = success_step['is_last'] or success_step['reward'] > 0
+                image = get_libero_image(success_step['observation'], 224, key='agentview_image')
+                image = preprocess_image(image)
+                wrist_image = get_libero_image(success_step['observation'], 224, key='robot0_eye_in_hand_image')
+                wrist_image = preprocess_image(wrist_image)
 
                 new_episode.append({
                     'observation': {
-                        'image': success_step['observation']['agentview_image'].numpy(),
-                        'wrist_image': success_step['observation']['robot0_eye_in_hand_image'].numpy(),
+                        'image': np.array(image),
+                        'wrist_image': np.array(wrist_image),
                         'state': success_step['observation']['robot0_proprio-state'].numpy(),
                     },
-                    'chosen_action': success_step['action'].numpy(),
-                    'rejected_action': false_action_steps[i].numpy(),
+                    'chosen_action': success_step['action'].numpy().astype(np.float32),
+                    'rejected_action': false_action_steps[i].numpy().astype(np.float32),
                     'discount': 1.0,
                     'reward': success_step['reward'].numpy(),
                     'is_first': success_step['is_first'].numpy(),
@@ -169,6 +231,10 @@ class LiberoDpoPairDataset(tfds.core.GeneratorBasedBuilder):
                     'language_instruction': language_instruction,
                     'language_embedding': language_embedding,
                 })
+
+            # NOTE: the final state is fake [0,0,0], so we remove it.
+            assert np.all(new_episode[-1]['chosen_action'] == np.zeros(7)), f'Assume the last action is zero but got: {new_episode[-1]["chosen_action"]}'
+            new_episode.pop(-1)
 
             # create output data sample
             sample = {
